5671e5757740af4b7aa1a39d204938439d31fcbf
Micheala-H <mholthus17@gmail.com>
2023-06-08 20:33:13 -0500

    added code to contollers and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,12 @@
-const User = require('../models/User');
+const { User }  = require('../models');
 
 module.exports= {
+    getUsers(req, res){
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
+
     createUser(req, res) {
         User.create(req.body)
         .then((user))
